74bd224a3885468f0b43f153a664262af3bcf0ac
snickyyy <leon12345671234@gmail.com>
2024-10-17 22:07:44 +0200

    end setup project

---
+++ src/config/settings/dev.py
@@ -0,0 +1,20 @@
+from config.settings.base import *  # NOQA
+
+SECRET_KEY = "django-insecure-6qxpqn(kzeka89cf=ucx&nctbet_2z#%+grri8(8l5_xqw0n9v"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS += []  # NOQA
+
+INSTALLED_APPS += []  # NOQA
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",  # NOQA
+    }
+}
+
+STATIC_URL = "static/"
--- src/manage.py
+++ src/manage.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
+import subprocess
 import sys
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
+    if sys.argv[1].lower() == "test":
+        subprocess.run(["black", "--config", "../.black.toml", "."])
+        subprocess.run(["isort", "."])
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
